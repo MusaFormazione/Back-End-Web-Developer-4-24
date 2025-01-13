0f5b7fc4cfdef1b08a1bef7d74f933e86169a9c7
captain-mike <info@grafikweb.it>
2025-01-13 13:57:10 +0100

    destrutturazione da verificare

--- L4/operatori-avanzati.php
+++ L4/operatori-avanzati.php
@@ -28,3 +28,35 @@ function somma(...$numeri){
 echo '<pre>';
 print_r(somma(2,5,3,4,5,6,7,8));
 echo '</pre>';
+
+//destrutturazione
+echo '<h2>Destrutturazione</h2>';
+
+$numeri = [1,2,3];
+[$a, $b, $c] = $numeri;//Se i tassi ristretta, consigliata 
+list($a, $b, $c) = $numeri;//Altra sintassi valida, ma non consigliata.
+echo "Valori destrutturati: a:$a, b:$b, c:$c";
+
+//destrutturazione array associativo
+$pizza = [
+    'gusto' => 'Margherita',
+    'prezzo' => 5
+];
+
+['gusto' => $gusto, 'prezzo' => $prezzo] = $pizza;
+
+echo "La pizza $gusto costa $prezzo €";
+
+
+//Destrutturazione con valore predefinito. 
+//da verificare
+// $pizza = [
+//     'gusto' => 'Margherita',
+//     'prezzo' => 5
+// ];
+
+// ['gusto' => $gustoPizza, 'prezzo' => $prezzoPizza, 'prezzoSconto' => $prezzoScontato ?? 4.5] = $pizza;
+
+// echo "La pizza $gustoPizza costa $prezzoPizza euro ma è in sconto a $prezzoScontato euro";
+
+
