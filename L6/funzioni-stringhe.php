39734b541afd2519f6fb7745eb418d6e0d640a3b
captain-mike <info@grafikweb.it>
2025-01-15 12:17:22 +0100

    funzioni manipolazione stringhe

--- L6/funzioni-stringhe.php
+++ L6/funzioni-stringhe.php
@@ -50,3 +50,70 @@ $stringa4 = "Hello World!";
 echo "Originale: $stringa4 $nl";
 echo "Dopo la sostituzione: " . str_replace('World','Mondo',  $stringa4) . $nl;
 echo "Dopo la sostituzione caseinsensitive: " . str_ireplace('world','Mondo',  $stringa4) . $nl;
+
+//Suddivisione di una stringa in una re di stringhe. 
+echo "<h3>explode/implode</h3>\n";
+
+$stringa5 = "Mario,Rossi,30";
+$arrayDaStringa5 = explode(',',$stringa5);
+
+echo "Originale: $stringa5 $nl";
+echo "Dopo l'utilizzo di explode con separatore \",\": $nl"; 
+var_dump($arrayDaStringa5);
+
+//posso destrutturare
+[$nome, $cognome, $anni] = $arrayDaStringa5;
+echo "Valori destrutturati => nome : $nome, cognome: $cognome, anni: $anni";
+
+
+echo $nl;
+echo $nl;
+
+
+echo "Originale: $nl";
+var_dump($arrayDaStringa5);
+
+echo "$nl Dopo l'utilizzo di implode $nl"; 
+
+echo implode(", ",$arrayDaStringa5);
+
+//Controllo se una stringa contiene un'altra stringa 
+echo "<h3>str_contains</h3>\n";
+
+$stringa6 = "Questo è un esempio in PHP";
+
+echo "Cerco js nella frase: \"$stringa6\", risultato:";
+var_dump(str_contains($stringa6, 'js'));
+echo $nl;
+
+echo "Cerco PHP nella frase: \"$stringa6\", risultato:";
+var_dump(str_contains($stringa6, 'PHP'));
+echo $nl;
+
+//Cerco una stringa all'inizio di un'altra stringa. 
+echo "<h3>str_starts_with</h3>\n";
+$stringa7 = "Questo è un esempio in PHP";
+
+echo "Cerco \"Questo\" all'inizio della frase: \"$stringa7\", risultato:";
+var_dump( str_starts_with($stringa7, 'Questo'));
+echo $nl;
+
+
+echo "Cerco \"PHP\" all'inizio della frase: \"$stringa7\", risultato:";
+var_dump( str_starts_with($stringa7, 'PHP'));
+echo $nl;
+
+
+//Cerco una stringa alla fine di un'altra stringa
+echo "<h3>str_ends_with</h3>\n";
+$stringa8 = "Questo è un esempio in PHP";
+
+echo "Cerco \"Questo\" alla fine della frase: \"$stringa8\", risultato:";
+var_dump( str_ends_with($stringa8, 'Questo'));
+echo $nl;
+
+
+echo "Cerco \"PHP\" alla fine della frase: \"$stringa8\", risultato:";
+var_dump( str_ends_with($stringa8, 'PHP'));
+echo $nl;
+
